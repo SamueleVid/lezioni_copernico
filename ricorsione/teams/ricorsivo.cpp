3c9da100b175c56f360c6c07f35bb3a3885a3554
Samuele Vidoni <86987754+SamueleVid@users.noreply.github.com>
2026-04-08 21:41:12 +0200

    Add recursive function for calculation in ricorsivo.cpp
    Implement recursive function to calculate result based on inputs A and B.

---
+++ ricorsione/teams/ricorsivo.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int res(int A, int B) {
+    if (A == 0) return 0;
+    if (B == 0) return 0;
+    if (A + B < 4) return 0;
+
+    if (A < B) {
+        swap(A, B);
+    }
+
+    if (A == B) {
+       return 1 + res(A - 2, B - 2); 
+    }
+    else {
+        return 1 + res(A - 3, B - 1);
+    }
+}
+
+int main() {
+    int A; cin >> A;
+    int B; cin >> B;
+
+    cout << res(A, B) << endl;
+}
